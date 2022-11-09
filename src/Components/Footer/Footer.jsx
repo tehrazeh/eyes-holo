efb0715f379cbcbf0480dd37bd934552154797e7
tehrazeh <danilatolstoukhov@gmail.com>
2022-11-08 22:51:49 -0500

    Changed colors of pages, refactored footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,9 @@ import styles from './Footer.module.scss'
 const Footer = () => {
     return (
         <div className={styles.Footer}>
-        <p>This is Footer</p>
+        <p>2022, tehrazeh dev.</p>
+        <p>App uses <a href='https://www.opendota.com/'>OpenDota API</a> and content from Dota 2 made by
+        <a href='https://www.valvesoftware.com/en/'> Valve.</a></p>
         </div>
     )
 }
