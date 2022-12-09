5acb77ed86697902b4f6823d4b98932c4a110a4d
tehrazeh <danilatolstoukhov@gmail.com>
2022-12-08 23:30:28 -0500

    Designed home page

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,8 +1,20 @@
+import styles from './Main.module.scss'
+import { Link } from 'react-router-dom'
 const Main = () => {
     return (
-        <>
-        <p>This is Main Section</p>
-        </>
+        <div className={styles.homeContainer}>
+            <p>Welcome to <b>eyes holo</b></p>
+        <p>Discover new details about Dota 2 heroes and items</p>
+        <img src={process.env.PUBLIC_URL + '/logo.png'}/>
+        <div className={styles.buttons}>    
+            <Link to='/Heroes'>
+            <button className={styles.pulse}>Heroes</button>
+            </Link>
+            <Link to='/Items'>
+            <button className={styles.pulse}>Items</button>
+            </Link>
+        </div>
+        </div>
     )
 }
 
